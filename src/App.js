e60c0b6070bedfddef8524c04b2d313659b08ae7
Leslie Thompson <lcrthompson@gmail.com>
2019-12-18 20:14:35 -0500

    Project notes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import "./App.css";
 import QuoteGenerator from "./components/QuoteGenerator/QuoteGenerator";
 import Weather from "./components/Weather/Weather";
 import Nav from "./components/Nav/Nav";
+
+// Things to add to this project:
+// - Time and date api
+// - Weather api
+// - Link to favorite sites?
+// - Add memes and videos that make me laugh lol
+// - Make api with own quotes? (later update)
 function App() {
   return (
     <div className="App">
