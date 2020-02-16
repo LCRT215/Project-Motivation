8a4aebfbcb26eb54c622c315fa9b06186e72be61
Leslie Thompson <lcrthompson@gmail.com>
2020-02-16 14:42:55 -0500

    update

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Nav from "./components/Nav/Nav";
 import TodoList from "./components/TodoList/TodoList";
 
 // Things to add to this project:
-// Make into a home page replacer? 
+// Make into a home page replacer?
 // - Make api with own quotes
 // - Weather api
 // - Link to favorite sites?
 // - Add Auth so user can add their own motivational quotes?? (firebase or Oauth?)
-// - Add feedback componen
+// - Add feedback component
 
 function App() {
   return (
