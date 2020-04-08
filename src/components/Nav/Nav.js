ced5768d0d7a9e268ce9624e6e9fdcb7a72a6c3f
Leslie Thompson <lcrthompson@gmail.com>
2020-04-08 18:40:03 -0400

    nav updates

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "../Nav/Nav.css";
 import DateTime from "./Date";
+import Morning from "../Nav/svg/001-window.svg";
+import Night from "../Nav/svg/002-curtain.svg";
+import Day from "../Nav/svg/003-window-1.svg";
 
 // import Weather from "../Weather/Weather";
 // import Clock from "react-live-clock";
@@ -9,13 +12,23 @@ function NavBar() {
   var date = new Date();
   var hrs = date.getHours();
   var greeting;
+  var img;
 
-  if (hrs < 12) greeting = "Good Morning";
-  else if (hrs >= 12 && hrs <= 17) greeting = "Good Afternoon";
-  else if (hrs >= 17 && hrs <= 24) greeting = "Good Evening";
+  if (hrs < 12) {
+    greeting = "Good Morning";
+    img = Morning;
+  } else if (hrs >= 12 && hrs <= 17) {
+    greeting = "Good Afternoon";
+    img = Day;
+  } else if (hrs >= 17 && hrs <= 24) {
+    greeting = "Good Evening";
+    img = Night;
+  }
+  document.write(img);
 
   return (
-    <div className="nav-container">
+    <div className="navContainer">
+      <img className="navImg" src={img} />
       <p className="navGreeting">{greeting}</p>
       <div className="dateTime">
         <DateTime />
