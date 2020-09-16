f4cd146b12ee0be867257eb88430e4491779e5ac
Leslie Thompson <lcrthompson@gmail.com>
2020-09-16 14:29:20 -0400

    update

--- src/components/Signature/Signature.js
+++ src/components/Signature/Signature.js
@@ -4,7 +4,7 @@ import "../Signature/Signature.css";
 function Signature() {
   return (
     <div className="signature-container">
-      <p className="text line1">Love, </p>
+      <p className="text line1">- Love, </p>
       <p className="text  line2"> Leslie</p>
     </div>
   );
